e590932df6db729de49b83266a5dd93da8057a4d
pashamakhilkumarreddy <pashamakhilkumarreddy@gmail.com>
2020-09-28 00:45:24 +0530

    misc fixes

--- src/first_non_repeating_char.py
+++ src/first_non_repeating_char.py
@@ -19,8 +19,18 @@ def first_non_repeating_char_sol(s: str) -> int:
       return s.index(k)
   return -1
 
+def first_non_repeating_char_sol_three(s: str) -> int:
+  for i, v in enumerate(s):
+    if (s.count(v) == 1):
+      return i
+  return -1
+
 print(first_non_repeating_char('Gwen Stacy Benjamin G'))
 print(first_non_repeating_char('alphabet'))
 
 print(first_non_repeating_char_sol('Gwen Stacy Benjamin G'))
 print(first_non_repeating_char_sol('alphabet'))
+
+print(first_non_repeating_char_sol_three('gwenstacy'))
+print(first_non_repeating_char_sol_three('alphabet'))
+print(first_non_repeating_char_sol_three('benjaminbyrnison'))
